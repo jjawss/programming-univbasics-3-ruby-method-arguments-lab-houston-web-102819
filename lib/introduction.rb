7d3cf3486ddd3ea3937a0a30214356db1cb46093
David Jaworski <jaws360@gmail.com>
2019-10-11 21:47:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -13,6 +13,7 @@ introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program #{language}."
 end
 
+introduction_with_language_optional(name, language = "Ruby")
 
 
 
